3c908b3b3c9c10d99ccfbb3587786ead6cab6086
Max Yeamans <max.yeamans@gmail.com>
2018-05-30 05:29:08 -0700

    Just adding the bare minimum of code to test some functionality for now.

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    // Leaving this blank for copy/pasting later
+    var QuizItem1 = {
+        "Question": "Question text",
+        "Answers": {
+            "Answer1": "Answer 1 here",
+            "Answer2": "Answer 2 here",
+            "Answer3": "Answer 3 here",
+            "Answer4": "Answer 4 here"
+        },
+        "CorrectAnswer": "Correct Answer here"
+    }
+
+    var QuizItems = [QuizItem1];
+
+    /*
+    I can create a bunch of quiz items, add them all to the QuizItems array, and then have a function
+    that accepts a quiz item as an argument. Once the function is set up, I'll set up a for loop that
+    iterates through the QuizItems array.
+    */
+
+    function DisplayQuizItem( item ) {
+        $("#display-area").append
+    }
+});
